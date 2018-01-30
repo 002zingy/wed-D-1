8be9b29c9cf89d0839cc78ceed97e4eaaadc8aa2
002zingy <002meamisha@gmail.com>
2018-01-30 12:11:36 +0530

    Add files via upload

---
+++ footer_vertex.php
@@ -0,0 +1,44 @@
+<?php
+echo '<div class="container-fluid" style="background-color:darkgray;">
+<div class="row">
+<div class="col-md-4" style="height:300px;">
+<ul class="footer1" style="padding-top:10px;">
+<li><a href="http://www.vertexclasses.in"> goto home </a></li>
+<li><a href="crashcourse.php"> crash course </a></li>
+
+</ul>   
+
+</div>
+<div class="col-md-4" style="height:300px;">
+<ul class="footer1" style="padding-top:10px;">
+<li><a href="https://www.facebook.com/vertexclass/" target="_blank"> facebook </a></li>
+
+
+<li><a href="https://www.google.co.in/maps/dir/\'\'/vertex+classes+dhanbad/@23.8005452,86.3658246,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x39f6bcbc30624bbd:0xe33d1d44d3cd58a!2m2!1d86.435865!2d23.800561" target="_blank"> find us on map </a></li>
+
+</ul>   
+
+</div>
+<div class="col-md-4" style="height:300px;">
+<ul class="footer1" style="padding-top:10px;">
+<li><a href="#"> contact us</a></li>
+
+</ul> 
+<address style="padding-left:40px;font-size:20px;">
+vertex classes<br> 
+Visit us at:<br>
+behind SSLNT College,<br>
+above HP GAS Agency<br>
+Hirapur, Dhanbad<br>
+vertexclassesinfo@gmail.com<br>
++918092198724/25<br>
+India
+</address>  
+
+</div>
+</div>
+<div class="row"> 
+<div class="col-md-12" style="text-align:center;padding-bottom:20px;"> &copy; copyright website of vertex classes, 2015-17 </div>
+</div>
+</div>';
+?>
---
+++ index.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>vertex classes</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/style_topnav.css">
+<link rel="stylesheet" href="css/style_footer.css">
+<link rel="stylesheet" href="css/style_vertex.css">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<style>
+.carousel-inner > .item > img, .carousel-inner > .item > a > img {
+	width:100%;
+	margin:auto;
+}
+.subject{ 
+        border-radius:50%;
+		background-color:#ffffff;
+}
+</style>
+</head>
+<body>
+<?php include 'nav_vertex.php'; ?>
+
+<div class="container-fluid" style="padding:0px; margin-top:0px;">
+<div id="mycarousel" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+<li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+<li data-target="#mycarousel" data-slide-to="1"></li>
+<li data-target="#mycarousel" data-slide-to="2"></li>
+</ol>
+<div class="carousel-inner" role="listbox">
+<div class="item active">
+<img src="images/1.jpg" alt="slide1" width="1080" height="300">
+<div class="carousel-caption">
+<h3>We have best mentors</h3>
+<p>Join today for better future</p>
+</div>
+</div>
+
+<div class="item">
+<img src="images/2.jpg" alt="slide1" width="1080" height="300">
+<div class="carousel-caption">
+<h3>Initiative of IITians(ISMities)</h3>
+<p>Assure your success with best mentors</p>
+</div>
+</div>
+
+<div class="item">
+<img src="images/3.jpg" alt="slide1" width="1080" height="300">
+<div class="carousel-caption">
+<h3>Contact us to join</h3>
+<p>We work for you better future</p>
+</div>
+</div>
+
+
+
+</div>
+
+</div>
+</div>
+
+
+<!-- best faculty in dhanbad -->
+<div style="width:100%; background-color: #d3c4d4;height:200px;margin: 10px 0;">
+<h1 class="text-center" style="font-size:4vw; color:black; padding:10px;">
+Vertex Classes announces Crash Course and Test Series
+</h1>
+<h1 class="text-center" style="font-size:2vw; color:black; padding:10px;">
+<a href="crashcourse.php">Click for more details</a>
+</h1>
+
+</div>
+
+<hr />
+<br />
+<div class="container-fluid">
+<div > <h1 style="margin:auto; text-align:center;padding-bottom:18px;"> We have best faculty team in dhanbad </h1> </div>
+<div class="row">
+<div class="col-md-4" style=" ">
+
+<div class="subject" style="margin:auto; background-color:#85DB17;color:white; width:150px;height:150px; text-align:center;vertical-align:middle;font-size:40px;line-height:150px;" > Physics</div>
+<ul style="list-style:none;" class="faculty">
+<li><h1>Ashish Shah (HOD)</h1> <h3> B Tech. - IIT(ISM) Dhanbad</h3> </li>
+
+<li><h1>Abhishek Singh</h1> <h3> B Tech. - IIT(ISM) Dhanbad</h3> </li>
+
+<li><h1>Yatendra Singhal</h1> <h3> B Tech. - IIT(ISM) Dhanbad</h3> </li>
+</ul>
+
+
+</div>
+<div class="col-md-4" style="">
+<div class="subject" style="margin:auto; background-color:#85DB17;color:white; width:150px;height:150px; text-align:center;vertical-align:middle;font-size:50px;line-height:150px;" >Maths </div>
+
+<ul style="list-style:none;" class="faculty">
+<li><h1>Md. Irfan (HOD)</h1> <h3> B Tech. - IIT(ISM) Dhanbad</h3> </li>
+
+<li><h1>Kartike Shankar</h1> <h3> B Tech. - IIT(ISM) Dhanbad</h3> </li>
+</ul>
+
+ </div>
+<div class="col-md-4" style=""> 
+<div class="subject" style="margin:auto; background-color:#85DB17;color:white; width:150px;height:150px; text-align:center;vertical-align:middle;font-size:35px;line-height:150px;" >Chemistry </div>
+<ul style="list-style:none;" class="faculty">
+<li><h1>Indrajeet Verma(HOD)</h1> <h3> B Tech. - IIT(ISM) Dhanbad</h3> </li>
+
+<li><h1>Rahul Phulwaria</h1> <h3> B Tech. - IIT(ISM) Dhanbad</h3> </li>
+
+<li><h1>Md. Tarique</h1> <h3> B Tech. - IIT(ISM) Dhanbad</h3> </li>
+</ul>
+</div>
+</div>
+</div>
+
+<!-- testimonials -->
+<hr />
+<br />
+<div class="container-fluid">
+<div > <h1 style="margin:auto; text-align:center;padding-bottom:18px;"> What students have to say about us </h1> </div>
+<div class="row">
+<div class="col-md-4" style="height:500px; background-color:white;">
+
+<div  style="margin:auto; width:150px;height:150px; text-align:center;vertical-align:middle;font-size:40px;line-height:150px;" ><img src="images/t1.jpg" alt="student1" style=" width:150px;height:150px;vertical-align:middle; border-radius:50%;margin:0px; padding:0px;border: 0px solid black" /> </div>
+<div style="text-align:center;">
+<h1 style="margin:10px 20px;">Altaf Ansari</h1>
+<h3 style="margin:10px 20px;"> My teachers helped me very much during my classes even if I ask any doubt many times till I understand the concept behind it  </h3>
+
+</div>
+</div>
+<div class="col-md-4" style="height:500px; background-color:white;">
+<div  style="margin:auto; width:150px;height:150px; text-align:center;vertical-align:middle;font-size:40px;line-height:150px;" ><img src="images/t2.jpg" alt="student1" style=" width:150px;height:150px;vertical-align:middle; border-radius:50%;margin:0px; padding:0px;border: 0px solid black" /> </div>
+<div style="text-align:center;">
+<h1 style="margin:10px 20px;">Karishma Verma</h1>
+<h3 style="margin:10px 20px;"> I always liked the way our teachers used to illustrate any topic in the class. As they have also qualified IIT, they understand and solve our problems easily </h3>
+</div>
+ </div>
+<div class="col-md-4" style="height:500px; background-color:white;">
+<div  style="margin:auto; width:150px;height:150px; text-align:center;vertical-align:middle;font-size:40px;line-height:150px;" ><img src="images/t3.jpg" alt="student1" style=" width:150px;height:150px;vertical-align:middle; border-radius:50%;margin:0px; padding:0px;border: 0px solid black" /> </div>
+<div style="text-align:center;">
+<h1 style="margin:10px 20px;">Anup Kr. Dasaundhi</h1>
+<h3 style="margin:10px 20px;">Teacher - student relationship is very good at vertex classes as our teachers never got irritated even if we asked any problem too many times till we understood the concept</h3>
+</div>
+</div>
+</div>
+</div>
+<?php include 'footer_vertex.php'; ?>
+</body>
+</html>
